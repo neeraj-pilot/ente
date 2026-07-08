759d429a8ccf9f81da464669b7a6a887b613f696
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-07-08 21:09:08 +0530

    Fallback shared strings localization

--- mobile/packages/strings/lib/extensions.dart
+++ mobile/packages/strings/lib/extensions.dart
@@ -1,11 +1,16 @@
 import 'package:ente_strings/l10n/strings_localizations.dart';
+import 'package:ente_strings/l10n/strings_localizations_en.dart';
 import 'package:flutter/widgets.dart';
 
 // Re-export the localizations for convenience
 export 'l10n/strings_localizations.dart';
 
+final _fallbackStringsLocalizations = StringsLocalizationsEn();
+
 /// Extension to easily access shared strings from any BuildContext
 extension EnteStringsExtension on BuildContext {
   /// Get the shared strings localizations for the current locale
-  StringsLocalizations get strings => StringsLocalizations.of(this);
+  StringsLocalizations get strings =>
+      Localizations.of<StringsLocalizations>(this, StringsLocalizations) ??
+      _fallbackStringsLocalizations;
 }
