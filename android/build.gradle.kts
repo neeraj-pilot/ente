72c68c59c6c48b58796c27be7a67393808e16c95
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-09-11 16:46:59 +0530

    Prepare Android build for shared native modules

---
+++ android/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("com.android.library") version "8.10.1" apply false
+    id("com.ncorti.ktfmt.gradle") version "0.27.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20" apply false
+}
