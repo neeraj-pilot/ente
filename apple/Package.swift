83525a2d20dfe08564f5a283d91ebe1e820c66ac
Neeraj Gupta <neeraj.gupta983@gmail.com>
2026-09-12 05:09:35 +0530

    Add Apple formatting checks

---
+++ apple/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "AppleTools",
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.63.0"),
+    ]
+)
